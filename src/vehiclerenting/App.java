1029cca87e9cd62643de108402b25bd5100962ec
Isuru Akalanka <jmiakalanka@gmail.com>
2024-10-15 11:34:44 +0530

    Add project files

---
+++ src/vehiclerenting/App.java
@@ -0,0 +1,18 @@
+
+package vehiclerenting;
+
+/**
+ *
+ * @author Isuru Akalanka
+ * @studentno CODSE232P-027
+ * @title Vehicle Renting System
+ * @version 1.0
+ * 
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Application is running..");
+    }
+    
+}
